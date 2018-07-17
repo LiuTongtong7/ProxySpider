a1ecd4d83f8dc5cb685ed6d2f17417b7c1724f00
LiuTongtong <LiuTongtong7@gmail.com>
2018-07-17 23:24:12 +0800

    modify sql

--- mysql.sql
+++ mysql.sql
@@ -3,6 +3,6 @@ CREATE TABLE IF NOT EXISTS proxies (
     ip CHAR(15),
     port INT,
     protocol CHAR(5),
-    ds DATETIME DEFAULT CURRENT_TIMESTAMP(),
+    ds TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (ip, port, protocol)
 );
